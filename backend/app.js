73bac29de0daa691796af4b16209bb8dda5b21d7
frausafiye <s_gulenc@outlook.com>
2021-03-04 15:21:40 +0100

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const mongoose=require("mongoose")
+const morgan = require("morgan")
+//routes:
+//middleware:
+const app=express()
+app.use(express.json())
+app.use(morgan("dev"))
+require('dotenv').config()
+
+//endpoints:
+
+//connection:
+mongoose.connect(process.env.MONGO_ATLAS,{useNewUrlParser:true,useUnifiedTopology:true},
+()=>console.log("connection established between app and mongodb"))
+mongoose.connection.on("error",()=>console.log("Error found while connecting"))
+
+
+//ERROR HANDLING:
+app.use((req,res,next)=>{
+  let error=new Error("no matching routes found")
+  error.status=404
+  next(error)
+})
+
+//UNIVERSAL ERROR HANDLING:
+app.use((err,req,res,next)=>{
+  res.status(err.status||500)
+  res.send({success:false,message:err.message})
+})
+
+
+app.listen(3000||process.env.PORT, ()=>console.log("server is running"))
