545f39fc9e2cfa5f2d1bcf2a737fc6292ecc79c5
Irina <irinagmarques@hotmail.com>
2021-03-09 13:58:59 +0100

    react-app fixed

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -4,7 +4,7 @@ export default function Register() {
     return (
         <div>
             <h1>Registration Page</h1>
-            <button>Register as school</button>
+            <button>Create a school</button>
             <button>Register as teacher</button>
         </div>
     )
