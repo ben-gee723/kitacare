c21d49307d30344c5e57e203e6812b277a4454fa
Irina <irinagmarques@hotmail.com>
2021-03-15 14:05:49 +0100

    link kgRegister

--- frontend/src/components/KgRegister.js
+++ frontend/src/components/KgRegister.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link} from "react-router-dom"
 
 export default function KgRegister() {
   const [user, setUser] = useState({
