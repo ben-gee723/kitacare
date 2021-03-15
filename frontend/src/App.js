5b635c0b8c79ec26581ec57be484cee1b4ad3ab4
Irina <irinagmarques@hotmail.com>
2021-03-15 11:27:42 +0100

    manager register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,57 @@
-import { Route, Link, Switch, withRouter, HashRouter } from "react-router-dom";
+import { Route, Link, Switch, withRouter } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Home from "./components/Home";
 import Register from "./components/Register";
 import KgRegister from "./components/KgRegister";
 import TeacherRegister from "./components/TeacherRegister";
+import ManagerRegister from "./components/ManagerRegister";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <HashRouter>
-    <div className='App'>
-      <h1>React App</h1>
-      <ul>
-        {/* Link * --> add ternary operator with isLogin */}
-        <Link to='/'>
-          <li>Home</li>
-        </Link>
-        <Link to='/register'>
-          <li>Register</li>
-        </Link>
-        <Link to='/kgregister'>
-          <li>RegisterKita</li>
-        </Link>
-        <Link to='/tregister'>
-          <li>RegisterTeacher</li>
-        </Link>
-        <Link to='/login'>
-          <li>Login</li>
-        </Link>
-        <Link to='/logout'>
-          <li>Logout</li>
-        </Link>
-      </ul>
+ 
+      <div className='App'>
+        <h1>React App</h1>
+        <ul>
+          {/* Link * --> add ternary operator with isLogin */}
+          <Link to='/'>
+            <li>Home</li>
+          </Link>
+          <Link to='/register'>
+            <li>Register</li>
+          </Link>
+          <Link to='/kgregister'>
+            <li>RegisterKita</li>
+          </Link>
+          <Link to='/tregister'>
+            <li>RegisterTeacher</li>
+          </Link>
+          <Link to='/mregister'>
+            <li>ManagerRegister</li>
+          </Link>
+          <Link to='/login'>
+            <li>Login</li>
+          </Link>
+          <Link to='/logout'>
+            <li>Logout</li>
+          </Link>
+        </ul>
 
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/register' component={Register} />
-        <Route path='/kgregister' component={KgRegister} />
-        <Route path='/tregister' component={TeacherRegister} />
-        <Route path='/login' component={Login} />
-        <Route path='/logout' component={Logout} />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
-    </HashRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/register' component={Register} />
+          <Route path='/kgregister' component={KgRegister} />
+          <Route path='/tregister' component={TeacherRegister} />
+          <Route path='/mregister' component={ManagerRegister} />
+          <Route path='/login' component={Login} />
+          <Route path='/logout' component={Logout} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+  
   );
 }
 
-export default App;
+export default withRouter(App);
--- frontend/src/components/ManagerRegister.js
+++ frontend/src/components/ManagerRegister.js
@@ -1,9 +1,115 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function ManagerRegister() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    birthday: "",
+    phoneNumber: "",
+    email: "",
+    address: "",
+    street:"",
+    number:"",
+    city:"",
+    postcode:"",
+    kgName: "",
+    groupName: "",
+    password: "",
+    role: "Admin",
+  });
+
+  const submitForm = e => {
+    e.preventDefault();
+  };
+
+  const grabValue = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        <label>
+          First Name :
+          <input
+            type='text'
+            name='firstName'
+            placeholder='First Name'
+            onChange={grabValue}
+          />
+        </label>
+        <br />
+
+        <label>
+          Last Name :
+          <input
+            type='text'
+            name='lastName'
+            required
+            placeholder='Last Name'
+            onChange={grabValue}
+          />
+        </label>
+        <br />
+
+        <label>
+          Email :{" "}
+          <input
+            type='email'
+            name='email'
+            required
+            placeholder='E-mail'
+            onChange={grabValue}
+          />
+        </label>
+        <br />
+
+        <label>
+          Password :
+          <input
+            type='password'
+            name='password'
+            required
+            placeholder='Password'
+            onChange={grabValue}
+          />
+        </label>
+        <br />
+
+        <label>
+          Role :{" "}
+          <select name='role' id='role' onChange={grabValue} required>
+            <option value='User'>User</option>
+            <option value='Admin'>Admin</option>
+          </select>
+        </label>
+        <br />
+
+        <label>
+          Street :
+          <input
+            type='text'
+            name='street'
+            placeholder='Street'
+            onChange={grabValue}
+          />
+        </label>
+        <br />
+        <label>
+          City :
+          <input
+            type='text'
+            name='city'
+            placeholder='City'
+            onChange={grabValue}
+          />
+        </label>
+        <br />
+
+        <input type='submit' value='Register' />
+        <Link to="/"><input type='submit' value='Cancel' /></Link>
+      </form>
+    </div>
+  );
 }
