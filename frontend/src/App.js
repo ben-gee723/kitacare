500f076e6234cb44962aa37ed70874a6817376d9
Irina <irinagmarques@hotmail.com>
2021-03-15 14:55:16 +0100

    h1 app.js  deleted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,46 +11,43 @@ import NotFound from "./components/NotFound";
 
 function App() {
   return (
- 
-      <div className='App'>
-        <h1>React App</h1>
-        <ul>
-          {/* Link * --> add ternary operator with isLogin */}
-          <Link to='/'>
-            <li>Home</li>
-          </Link>
-          <Link to='/register'>
-            <li>Register</li>
-          </Link>
-          <Link to='/kgregister'>
-            <li>RegisterKita</li>
-          </Link>
-          <Link to='/tregister'>
-            <li>RegisterTeacher</li>
-          </Link>
-          <Link to='/mregister'>
-            <li>ManagerRegister</li>
-          </Link>
-          <Link to='/login'>
-            <li>Login</li>
-          </Link>
-          <Link to='/logout'>
-            <li>Logout</li>
-          </Link>
-        </ul>
+    <div className='App'>
+      <ul>
+        {/* Link * --> add ternary operator with isLogin */}
+        <Link to='/'>
+          <li>Home</li>
+        </Link>
+        <Link to='/register'>
+          <li>Register</li>
+        </Link>
+        <Link to='/kgregister'>
+          <li>RegisterKita</li>
+        </Link>
+        <Link to='/tregister'>
+          <li>RegisterTeacher</li>
+        </Link>
+        <Link to='/mregister'>
+          <li>ManagerRegister</li>
+        </Link>
+        <Link to='/login'>
+          <li>Login</li>
+        </Link>
+        <Link to='/logout'>
+          <li>Logout</li>
+        </Link>
+      </ul>
 
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/register' component={Register} />
-          <Route path='/kgregister' component={KgRegister} />
-          <Route path='/tregister' component={TeacherRegister} />
-          <Route path='/mregister' component={ManagerRegister} />
-          <Route path='/login' component={Login} />
-          <Route path='/logout' component={Logout} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-  
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/register' component={Register} />
+        <Route path='/kgregister' component={KgRegister} />
+        <Route path='/tregister' component={TeacherRegister} />
+        <Route path='/mregister' component={ManagerRegister} />
+        <Route path='/login' component={Login} />
+        <Route path='/logout' component={Logout} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
   );
 }
 
