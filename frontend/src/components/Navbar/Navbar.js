f78a83815cf137ffdbb006cad166b88d4647f88a
bartxs <b.soltysik90@gmail.com>
2021-03-18 11:20:37 +0100

    navbar update

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,14 +1,27 @@
 import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../../images/logo.svg";
 
 export default function Navbar() {
     return (
-        <div>
-            <div className='lg'>
-                <NavLink to='/'>
-                  <img src={logo} alt='lg' />
-                </NavLink>
-              </div>
-        </div>
+        <div className='nav'>
+          <div className='lg'>
+              <NavLink to='/'>
+                <img src={logo} alt='lg' />
+              </NavLink>
+              <div className='kita'>
+                Kitacare
+            </div>
+          </div>
+            
+            <div className='navbtn'>
+                <Link to='/register'>
+                  <button className="regbtn">Register</button>
+                </Link>
+                <Link to='/login'>
+                  <button className="loginbtn">Login</button>
+                </Link>
+            </div>
+        </div>              
     )
 }
