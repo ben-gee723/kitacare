b7b01b0aec17e862bb5aca0aa8b699426115e294
ben-gee723 <64844825+ben-gee723@users.noreply.github.com>
2021-03-22 11:52:17 +0100

    Update postControllers.js

--- backend/controllers/groupControllers/postControllers.js
+++ backend/controllers/groupControllers/postControllers.js
@@ -3,8 +3,7 @@ const GroupModel = require("../../model/groupModel")
 // addGroup
 exports.addGroup = async (req, res, next) => {
     try {
-        const group = await GroupModel(req.body)
-        group.save()
+        const group = await GroupModel.create(req.body);
         res.status(200).send({ success: true, group: group, message: "group saved into db" })
     }
     catch (err) {
