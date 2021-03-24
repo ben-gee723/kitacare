b6483cf5feb8fc8767d29f97e0c6a08a8731b86f
Irina <irinagmarques@hotmail.com>
2021-03-24 14:11:54 +0100

    usersRoute + calendarSchema

--- backend/model/calendarModel.js
+++ backend/model/calendarModel.js
@@ -2,12 +2,8 @@ const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 
 const EventSchema = new Schema({
-  startDate: {
-    type: Date,
-    lastActiveAt: "2020–01–01",
-    required: true,
-  },
-  endDate: { type: Date, lastActiveAt: "2020–01–01", required: true },
+  startDate: { type: Date,  required: true},
+  endDate: { type: Date, required: true },
   name: { type: String, required: true },
   creator: { type: String, required: false },
 });
