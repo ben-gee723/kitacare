8dd596bf237d41fbb098838ffb42c279da33d508
frausafiye <s_gulenc@outlook.com>
2021-03-29 09:14:43 +0200

    created a basic task for manager dashboard

---
+++ frontend/src/components/ManagerDashboard/ManagerDashboard.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function ManagerDashboard() {
+  return (
+    <div>
+      <h3>KG Name</h3>
+      <h4>Staff?</h4>
+      <ul>
+        <li>
+          <p>Manager A </p>
+          <button>remove</button>
+          <button>edit</button>
+        </li>
+        <li>
+          <p>Teacher B </p>
+          <button>remove</button>
+          <button>edit</button>
+        </li>
+        <button>add a new group</button>
+      </ul>
+      <h4>Groups</h4>
+      <ul>
+        <li>
+          <p>Lions</p>
+          <button>remove</button>
+          <button>edit</button>
+        </li>
+        <li>
+          <p>Crocodiles</p>
+          <button>remove</button>
+          <button>edit</button>
+        </li>
+        <button>add a new group</button>
+      </ul>
+      
+
+
+
+    </div>
+  )
+}
