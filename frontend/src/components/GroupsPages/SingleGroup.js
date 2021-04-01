c8ca8cc8ec84b9ce75e4ae2d6d9a01de10510758
Irina <irinagmarques@hotmail.com>
2021-04-01 12:11:11 +0200

    groups working properly

--- frontend/src/components/GroupsPages/SingleGroup.js
+++ frontend/src/components/GroupsPages/SingleGroup.js
@@ -4,59 +4,42 @@ import axios from "axios";
 import { MyContext } from "../../Container";
 
 export default function SingleGroup(props) {
-  const [deletedGroup, setDeleteGroup] = useState([]);
-  console.log(props.location)
+  const [showForm, setShowForm] = useState(false);
 
-  const handleEdit = id => {
-    <SingleGroupEdit />;
-  };
-  const handleDelete = id => {
-    axios(
-      `http://localhost:3001/groups/getSingleGroup/${props.location.state.group}`,
-      {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-      }
-    ).then(result => {
-      if (result.success) {
-        setDeleteGroup(result.deletedGroup);
-      } else {
-        console.log(result);
-      }
-    });
+  const handleEdit = (group) => {
+    setShowForm(!showForm);
+    props.history.push({ pathname: "/editgroup", state: { group: group } });
   };
 
   const group = props.location.state.group;
-
-  return ( 
+  return (
     <div>
-      <h1>Groups</h1>
-      <div>
-        <div key={group._id}>
-          <div>
-            <h3>{group.groupName}</h3>
-            <p>{group.description}</p>
-          </div>
-          <div>
-            <button
-              type='submit'
-              value='delete'
-              className='delete'
-              onClick={() => handleDelete(group.id)}
-            >
-              Delete
-            </button>
-            <button
-              type='submit'
-              value='edit'
-              className='edit'
-              onClick={() => handleEdit(group.id)}
-            >
-              Edit
-            </button>
+      {showForm && (
+        <div>
+          <SingleGroupEdit />
+        </div>
+      )}
+      <>
+        <h1>Groups</h1>
+        <div>
+          <div key={group._id}>
+            <div>
+              <h3>{group.groupName}</h3>
+              <p>{group.description}</p>
+            </div>
+            <div>
+              <button
+                type='submit'
+                value='edit'
+                className='edit'
+                onClick={() => handleEdit(group)}
+              >
+                Edit
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     </div>
   );
 }
--- frontend/src/components/GroupsPages/SingleGroupEdit.js
+++ frontend/src/components/GroupsPages/SingleGroupEdit.js
@@ -4,29 +4,34 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 export default function SingleGroupEdit(props) {
-  const [group, setGroup] = useState([]);
+  const [deletedGroup, setDeleteGroup] = useState([]);
+  const [editedGroup, setEditedGroup] = useState([]);
+  const group = props.location.state.group;
 
-  useEffect(() => {
-    axios(`http://localhost:3001/groups/getSingleGroup/${props.location.state.id}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    }).then(result => {
+  const handleDelete = (props) => {
+    axios(
+      `http://localhost:3001/groups/getSingleGroup/${props.location.state.group}`,
+      {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then(result => {
       if (result.success) {
-        setGroup(result.group);
+        setDeleteGroup(result.deletedGroup);
       } else {
         console.log(result);
       }
     });
-  }, []);
+  };
 
-  const handleEdit = (id, e) => {
+  const handleEdit = (props, e) => {
     e.preventDefault();
-    axios(`http://localhost:3001/groups/${props.location.state.id}`, {
+    axios(`http://localhost:3001/groups/${props.location.state.group}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
     }).then(result => {
       if (result.success) {
-        setGroup(result.group);
+        setEditedGroup(result.group);
       } else {
         console.log(result);
       }
@@ -34,105 +39,91 @@ export default function SingleGroupEdit(props) {
   };
 
   const editedValue = e => {
-    setGroup({ ...group, [e.target.name]: e.target.value });
+    setEditedGroup({ ...group, [e.target.name]: e.target.value });
   };
 
   return (
     <div className={styles.regForm}>
-      {group.map(group => {
-        return (
-          <form
-            className={styles.formContainer}
-            onSubmit={id => handleEdit(group.id)}
-            name='managerForm'
-            key='group.id'
+      <form
+        className={styles.formContainer}
+        onSubmit={id => handleEdit(group._id)}
+        name='managerForm'
+        key='group._id'
+      >
+        <div className='reg'>
+          <h1>Edit Group!</h1>
+        </div>
+
+        <div className='inputBox'>
+          <label className='details'>First name</label>
+          <br />
+          <input type='text' name='firstName' placeholder='First Name' />
+        </div>
+
+        <div className='inputBox'>
+          <label className='details'>Last name</label>
+          <br />
+          <input type='text' name='lastName' placeholder='Last Name' />
+        </div>
+
+        <div className='inputBox'>
+          <label className='details'>Birthday</label>
+          <br />
+          <input
+            type='date'
+            name='birthday'
+            placeholder='Birthday'
+            value='group.'
+          />
+        </div>
+        <div className='inputBox'>
+          <label className='details'>Phone number</label>
+          <br />
+          <input type='text' name='phoneNumber' placeholder='Phone Number' />
+        </div>
+
+        <div className='inputBox'>
+          <label className='details'>Email</label>
+          <br />
+          <input type='email' name='email' placeholder='E-mail' />
+        </div>
+        <div className={styles.address}>
+          <h3>Address:</h3>
+        </div>
+        <div className='inputBox'>
+          <label className='details'>Street</label>
+          <br />
+          <input type='text' name='street' placeholder='Street' />
+        </div>
+        <div className='inputBox'>
+          <label className='details'>Number</label>
+          <br />
+          <input type='text' name='number' placeholder='Number' />
+        </div>
+        <div className='inputBox'>
+          <label className='details'>City</label>
+          <br />
+          <input type='text' name='city' placeholder='City' />
+        </div>
+
+        <br />
+        <div className={styles.btnContainer}>
+          <Link to='/'>
+            <button className='cancel'>Cancel</button>
+          </Link>
+          <button type='submit' value='Edit' className='att'>
+            Submit
+          </button>
+          <button
+            type='submit'
+            value='delete'
+            className='delete'
+            onClick={() => handleDelete(group.id)}
           >
-            <div className='reg'>
-              <h1>Edit Group!</h1>
-            </div>
-
-            <div className='inputBox'>
-              <label className='details'>First name</label>
-              <br />
-              <input type='text' name='firstName' placeholder='First Name' />
-            </div>
-
-            <div className='inputBox'>
-              <label className='details'>Last name</label>
-              <br />
-              <input type='text' name='lastName' placeholder='Last Name' />
-            </div>
-
-            <div className='inputBox'>
-              <label className='details'>Birthday</label>
-              <br />
-              <input
-                type='date'
-                name='birthday'
-                placeholder='Birthday'
-                value='group.'
-              />
-            </div>
-            <div className='inputBox'>
-              <label className='details'>Phone number</label>
-              <br />
-              <input
-                type='text'
-                name='phoneNumber'
-                placeholder='Phone Number'
-              />
-            </div>
-
-            <div className='inputBox'>
-              <label className='details'>Email</label>
-              <br />
-              <input type='email' name='email' placeholder='E-mail' />
-            </div>
-            <div className={styles.address}>
-              <h3>Address:</h3>
-            </div>
-            <div className='inputBox'>
-              <label className='details'>Street</label>
-              <br />
-              <input type='text' name='street' placeholder='Street' />
-            </div>
-            <div className='inputBox'>
-              <label className='details'>Number</label>
-              <br />
-              <input type='text' name='number' placeholder='Number' />
-            </div>
-            <div className='inputBox'>
-              <label className='details'>City</label>
-              <br />
-              <input type='text' name='city' placeholder='City' />
-            </div>
-            <div className='inputBox'>
-              <label className='details'>Post code</label>
-              <br />
-              <input
-                type='number'
-                name='postcode'
-                required
-                placeholder='Postcode'
-              />
-            </div>
-            <div className='inputBox'>
-              <label className='details'>Password</label>
-              <br />
-              <input type='password' name='password' placeholder='Password' />
-            </div>
-            <br />
-            <div className={styles.btnContainer}>
-              <Link to='/'>
-                <button className='cancel'>Cancel</button>
-              </Link>
-              <button type='submit' value='Edit' className='att'>
-                Submit
-              </button>
-            </div>
-          </form>
-        );
-      })}
+            Delete
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
