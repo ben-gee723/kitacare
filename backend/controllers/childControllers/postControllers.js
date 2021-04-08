0741ba67b20b77b506fabaf3be6fad015dc580b0
ben <beatuscanis@hotmail.com>
2021-04-08 16:22:13 +0200

    child registration nearly done

--- backend/controllers/childControllers/postControllers.js
+++ backend/controllers/childControllers/postControllers.js
@@ -4,20 +4,48 @@ const ChildModel = require("../../model/childModel")
 // addChild
 exports.addChild = async (req, res, next) => {
     try {
-        console.log("something haha")
+        console.log("Find child")
+        console.log("this is the req body", req.body.emergencyContact[0])
+        console.log("this is the req body", req.body.emergencyContact[1])
         // ASYNC AWAIT
         const { lastName, firstName, birthday } = req.body
         const child = await ChildModel.findOne({ lastName: lastName, firstName: firstName, birthday: birthday })
         if (!child) {
-            const newChild = await ChildModel.create(req.body);
-            res.status(200).send({ success: true, child: newChild })
+            console.log("Child doesn't exist, attempted to create child");
+            const child = await ChildModel.create(req.body);
+            res.status(200).send({
+                success: true,
+                child: child,
+                message: "child saved into db"
+            })
         } else {
-            res.status(400).send({ success: false, message: "Child already exists in database" })
+            res.status(400).send({
+                success: false,
+                message: "Child already exists in database"
+            })
         }
 
+
+        /*
+            const child = await ChildModel.create(req.body);
+        if (child.emergencyContact.length !== 0) {
+            res.status(200).send({
+                success: true,
+                child: child,
+                message: "child saved into db"
+            })
+        } else {
+            res.status(400).send({
+                success: false,
+                message: "please insert information",
+                child: child
+            })
+        }
+        */
     }
     catch (err) {
-        console.log("something is wrong lol");
+        // console.log("Did not succeed");
+        console.log(err)
         next(err);
     }
 }
--- backend/model/childModel.js
+++ backend/model/childModel.js
@@ -11,33 +11,36 @@ const EmergencyContactSchema = new Schema({
 });
 
 const ChildSchema = new Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    birthday: { type: Date, required: true },
+    firstName: { type: String, required: false },
+    lastName: { type: String, required: false },
+    birthday: { type: Date, required: false },
     address: {
         type: AddressSchema,
         required: false,
     },
     kg: { ref: "kindergardens", type: mongoose.Schema.Types.ObjectId },
     img: { type: String },
-    allergies: { type: [{ to: String }], required: false },
-    dietaryNeeds: { type: [{ requirement: String }], required: false },
-    emergencyContact: [
-        { EmergencyContactSchema },
-        { EmergencyContactSchema }
-    ],
+    allergies: { type: [], required: false },
+    dietaryNeeds: { type: String, required: false },
+    // emergencyContact: {[
+    //     { Emergency1: EmergencyContactSchema },
+    //     { Emergency2: EmergencyContactSchema }
+    // ]},
+    emergencyContact: { type: [], required: false },
     attendance: [
         { date: Date }
     ]
 })
 
-ChildSchema.pre("validate", function (next) {
+/*
+    ChildSchema.pre("validate", function (next) {
     if (this.emergencyContact !== 0) {
         return next();
     }
     const error = new Error("please provide us with emergency contact details");
     next(error);
-});
+    });
+ */
 
 const ChildModel = mongoose.model("children", ChildSchema);
 
