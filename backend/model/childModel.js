e6a886bbf0bdf8c9aca82c51de066a4a5981309f
ben <beatuscanis@hotmail.com>
2021-04-12 11:01:22 +0200

    cleaned up the child model

--- backend/model/childModel.js
+++ backend/model/childModel.js
@@ -4,12 +4,6 @@ const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 const AddressSchema = require("./addressSchema");
 
-const EmergencyContactSchema = new Schema({
-    name: { type: String, required: false },
-    email: { type: String, required: false },
-    number: { type: String, required: false }
-});
-
 const ChildSchema = new Schema({
     firstName: { type: String, required: false },
     lastName: { type: String, required: false },
@@ -22,25 +16,11 @@ const ChildSchema = new Schema({
     img: { type: String },
     allergies: { type: [], required: false },
     dietaryNeeds: { type: String, required: false },
-    // emergencyContact: {[
-    //     { Emergency1: EmergencyContactSchema },
-    //     { Emergency2: EmergencyContactSchema }
-    // ]},
     emergencyContact: { type: [], required: false },
     attendance: [
         { date: Date }
     ]
 })
-
-/*
-    ChildSchema.pre("validate", function (next) {
-    if (this.emergencyContact !== 0) {
-        return next();
-    }
-    const error = new Error("please provide us with emergency contact details");
-    next(error);
-    });
- */
 
 const ChildModel = mongoose.model("children", ChildSchema);
 
