2d470c99b97eed9bb3c0bb7072b127ceb1986279
ben <beatuscanis@hotmail.com>
2021-04-17 09:08:17 +0200

    edits to Attendance.js

--- frontend/src/components/Tpage/Attendance/Attendance.js
+++ frontend/src/components/Tpage/Attendance/Attendance.js
@@ -12,15 +12,30 @@ import NotHere from './NotHere';
 export default function Attendance() {
     const [children, setChildren] = useState([]);
     const { kg, user } = useContext(MyContext);
-    const [attendance, setAttendance] = useState([]);
+    // const [attendance, setAttendance] = useState([]);
     // [{child:..., attendanceStatus: "here/notHere", date: ""}]
-    let date = new Date()
-    console.log(date)
+    let date = new Date().getFullYear() + "-" + ("0" + (new Date().getMonth() + 1)).slice(-2) + "-" + ("0" + new Date().getDate()).slice(-2)
+    // console.log(date)
+    console.log(children)
 
-    let hereChildren = attendance.length ? attendance.filter(obj => obj.attendanceInfo.attendanceStatus == "here") : [];
-    let notHereChildren = attendance.length ? attendance.filter((obj) => obj.attendanceInfo.attendanceStatus == "notHere") : [];
-    console.log(hereChildren)
-    console.log(notHereChildren)
+    let hereChildrenNew = children.filter(obj => obj.attendance.date == date);
+    // console.log("new here children", hereChildrenNew)
+    let notHereChildrenNew = children.map(child => {
+        if (child.attendance.length != 0) {
+            return child.attendance[0]
+        } else {
+            return
+
+        }
+
+    })
+    console.log("new notHere children", notHereChildrenNew)
+
+    let hereChildren = children.length ? children.filter(obj => obj.attendanceInfo == "here") : [];
+    let notHereChildren = children.length ? children.filter((obj) => obj.attendanceInfo == "notHere") : [];
+
+    // console.log(hereChildren)
+    // console.log(notHereChildren)
 
     // Getting the children from the kindergarten
     useEffect(() => {
@@ -34,12 +49,12 @@ export default function Attendance() {
             },
         })
             .then(result => {
-                console.log(result);
+                // console.log(result);
                 if (result.data.success) {
                     let groupChildren = result.data.allChildren.filter(obj =>
                         obj.group === user.group._id
                     )
-                    console.log(groupChildren)
+                    // console.log(groupChildren)
                     setChildren(groupChildren);
                 } else {
                     console.log(result);
@@ -47,30 +62,6 @@ export default function Attendance() {
             })
             .catch(err => console.log(err))
     }, []);
-
-    useEffect(() => {
-        if (children.length) {
-            console.log(children)
-            axios({
-                method: "GET",
-                url: `http://localhost:3001/child/getAttendanceOfChild/${user.group._id}`,
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-            })
-                .then(result => {
-                    console.log(result.data);
-                    if (result.data.success) {
-                        console.log(result.data.attendanceArr)
-                        setAttendance(result.data.attendanceArr);
-                    } else {
-                        console.log(result);
-                    }
-                })
-                .catch(err => console.log(err))
-        }
-    }, [children])
 
     const handleAttendance = (e, childId) => {
         e.preventDefault();
@@ -81,6 +72,8 @@ export default function Attendance() {
             obj[pair[0]] = pair[1];
         }
         console.log(obj)
+        // console.log(attendance)
+        // console.log(children)
 
 
         axios(`http://localhost:3001/child/updateAttendance/${childId}`, {
@@ -89,13 +82,11 @@ export default function Attendance() {
             data: obj
         }).then(result => {
             if (result.success) {
-                setAttendance(result.data.updatedAttendance);
+                // setAttendance(result.data.updatedAttendance);
             } else {
                 console.log(result);
             }
         });
-
-        console.log(obj)
     }
 
     return (
