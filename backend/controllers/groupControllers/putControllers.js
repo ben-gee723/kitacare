aedaf77e5db7cca9ee123e549465cb90268a7f6c
frausafiye <s_gulenc@outlook.com>
2021-04-22 10:51:20 +0200

    details

--- backend/controllers/groupControllers/putControllers.js
+++ backend/controllers/groupControllers/putControllers.js
@@ -1,19 +1,22 @@
+/** @format */
+
 const GroupModel = require("../../model/groupModel");
 
 // update group
 exports.updateGroup = async (req, res, next) => {
-    const { id } = req.params;
-    try {
-        const updatedGroup = await GroupModel.findByIdAndUpdate(id, req.body, {
-            new: true,
-        });
-        if (updatedGroup) {
-            res.status(200).send({ success: true, updatedGroup: updatedGroup })
-        } else {
-            res.status(400).send({ success: false, message: "no matching group found" })
-        }
+  const { id } = req.params;
+  try {
+    const updatedGroup = await GroupModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (updatedGroup) {
+      res.status(200).send({ success: true, updatedGroup: updatedGroup });
+    } else {
+      res
+        .status(400)
+        .send({ success: false, message: "no matching group found" });
     }
-    catch (err) {
-        next(er)
-    }
-}
+  } catch (err) {
+    next(err);
+  }
+};
